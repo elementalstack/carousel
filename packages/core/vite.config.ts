0cd5a5d62ce6d76ee6db6a83f82b0fa7bc5c9d9e
jose ricardo kremer polesi <josericardo.kremer@gmail.com>
2023-03-18 17:08:02 -0300

    chore: add vitest configuration to each package

---
+++ packages/core/vite.config.ts
@@ -0,0 +1,4 @@
+import { defineConfig } from 'vitest/config';
+import { viteBaseConfig } from '../../config/viteBase';
+
+export default defineConfig(viteBaseConfig);
--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -1,15 +1,14 @@
-import { defineConfig, mergeConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 import react from '@vitejs/plugin-react';
 import { viteBaseConfig } from '../../config/viteBase';
+import { mergeConfig } from 'vite';
 
-const config = mergeConfig(viteBaseConfig, {
+const config = defineConfig({
   test: {
-    setupFiles: '../../config/setupTest.ts',
+    setupFiles: ['./config/setupTest.ts'],
   },
-});
-
-export default defineConfig({
-  ...config,
   plugins: [react()],
 });
+
+export default mergeConfig(viteBaseConfig, config);
