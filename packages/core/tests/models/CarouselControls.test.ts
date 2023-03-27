8c806b670b8be834b6bc2a838cd58b594dd313eb
jose ricardo kremer polesi <josericardo.kremer@gmail.com>
2023-03-27 00:01:26 -0300

    test(carouselcontrols): add missing tests to CarouselControls
    closes #10

---
+++ packages/core/tests/models/CarouselControls.test.ts
@@ -0,0 +1,68 @@
+import CarouselControls from '../../src/models/CarouselControls';
+import { ICarouselState } from '../../src/models/CarouselState';
+
+class CarouselStateStub implements ICarouselState {
+  positions = [];
+  currentIndex = 0;
+}
+
+function makeSut() {
+  const carouselStateStub = new CarouselStateStub();
+
+  const sut = new CarouselControls(carouselStateStub);
+
+  return { sut, state: carouselStateStub };
+}
+
+describe('CarouselControls', () => {
+  describe('goToIndex', () => {
+    it('Should go to given index', () => {
+      const indexToGo = 10;
+
+      const { sut, state } = makeSut();
+
+      sut.goToIndex(indexToGo);
+
+      expect(state.currentIndex).toBe(indexToGo);
+    });
+  });
+
+  describe('goToNextIndex', () => {
+    it('Should go to next index', () => {
+      const { sut, state } = makeSut();
+
+      sut.goToNextItem();
+
+      expect(state.currentIndex).toBe(1);
+    });
+  });
+
+  describe('goToPrevItem', () => {
+    it('Should go to previous index', () => {
+      const { sut, state } = makeSut();
+
+      sut.goToPrevItem();
+
+      expect(state.currentIndex).toBe(-1);
+    });
+  });
+
+  it('Should update currentIndex correctly', () => {
+    const { sut, state } = makeSut();
+
+    sut.goToNextItem();
+    expect(state.currentIndex).toBe(1);
+
+    sut.goToPrevItem();
+    expect(state.currentIndex).toBe(0);
+
+    sut.goToIndex(2);
+    expect(state.currentIndex).toBe(2);
+
+    sut.goToNextItem();
+    expect(state.currentIndex).toBe(3);
+
+    sut.goToPrevItem();
+    expect(state.currentIndex).toBe(2);
+  });
+});
