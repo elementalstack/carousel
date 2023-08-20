10b72fa4d9f23329ae2ffe5bd2c5b22ad5514321
jose ricardo kremer polesi <josericardo.kremer@gmail.com>
2023-08-20 20:15:08 -0300

    test: add testing-library

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,6 +28,7 @@ module.exports = {
     "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": "off",
 		"unused-imports/no-unused-imports": "error",
+    "@typescript-eslint/no-unsafe-assignment": "warn",
 		"unused-imports/no-unused-vars": [
 			"warn",
 			{ "vars": "all", "varsIgnorePattern": "^_", "args": "after-used", "argsIgnorePattern": "^_" }
